94f486ad5bd502f67c27a2425afe3dae66a4a276
Zplaninic <zvonimir.planinic@gmail.com>
2020-06-18 10:49:07 +0200

    initial commit

---
+++ apps/dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-projects-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dashboard';
+}
